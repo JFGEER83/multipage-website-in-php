14f00e538847a6b56041f7c51cef84302ab24e95
Paul Henrot <henrot.paul@gmail.com>
2018-08-01 16:55:25 +0200

    réglage des différnts toggle hide, d-flex et transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,63 @@ let buttonAbout= document.getElementById("button-about");
 let buttonDoc= document.getElementById("button-doc");
 let buttonInfos= document.getElementById("button-infos");
 let buttonContact= document.getElementById("button-contact");
-buttonAbout.addEventListener("click",toggleTransition);
-buttonInfos.addEventListener("click",toggleTransition);
-buttonContact.addEventListener("click",toggleTransition);
-buttonDoc.addEventListener("click",toggleTransition);
-function toggleTransition(){
- /*     setTimeout(locationPage1, 1500); */
+let buttonsMenu= document.querySelectorAll(".button-menu--round");
+let section= document.querySelectorAll(".section");
+let sectionAbout= document.getElementById("section-about");
+let sectionDoc= document.getElementById("section-doc");
+let sectionInfos= document.getElementById("section-infos");
+let sectionContact= document.getElementById("section-contact");
+buttonAbout.addEventListener("click",clickAbout);
+buttonInfos.addEventListener("click",clickInfos);
+buttonContact.addEventListener("click",clickContact);
+buttonDoc.addEventListener("click",clickDoc);
+function toggleFlex(item){
+    item=item.classList.toggle("d-flex");
+}
+function toggleHide(item){
+    item=item.classList.toggle("hide");
+}
+function toggleTransition(item){
+ 
+    item=item.classList.toggle("transition");
+}
+function clickButtons(){
+    buttonsMenu.forEach(toggleHide);
     menuMain.classList.toggle("transition");
     headerMain.classList.toggle("transition");
-    buttonInfos.classList.toggle("transition");
     bodyMain.classList.toggle("transition"); 
-     
-      /* locationPage1();  */
+}
+ function clickInfos(){
+     clickButtons();
+    /*  section.forEach(toggleHide); */
+     buttonsMenu.forEach(toggleTransition);
+     section.forEach(toggleTransition);
+     buttonInfos.classList.toggle("hide");
+     sectionInfos.classList.toggle("hide");
  }
+ function clickContact(){
+     clickButtons();
+     buttonsMenu.forEach(toggleTransition);
+     buttonContact.classList.toggle("hide");
+ }
+ function clickDoc(){
+    clickButtons();
+    buttonsMenu.forEach(toggleTransition);
+    buttonDoc.classList.toggle("hide");
+ }
+ function clickAbout(){
+    clickButtons();
+    buttonsMenu.forEach(toggleTransition);
+    buttonAbout.classList.toggle("hide");
+ }
+function startPage(){
+    section.forEach(toggleFlex);
+    section.forEach(toggleHide);
+}
+startPage();
+
+
+/*     setTimeout(locationPage1, 1500); */
 /* function locationPage1(){
     document.location.href="page_about.html";
 } */
